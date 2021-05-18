570f633a3ba2b01b90681f43684a70ad2fe734b9
sw33tsoda <sw33tsoda@gmail.com>
2021-05-18 17:12:15 +0700

    update

--- src/components/BreedInformation.vue
+++ src/components/BreedInformation.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="breed-information" v-if="Object.keys(selectedBreed).length > 0">
-        <v-card
+        <div class="split">
+            <v-card
             class="mx-auto"
             max-width="344"
         >
@@ -47,12 +48,13 @@
             </v-simple-table>
 
             <v-card-actions>
-            <v-btn
+            <!-- <v-btn
                 color="orange lighten-2"
+                @click="getCatsByBreed"
                 text
             >
                 More of this breed
-            </v-btn>
+            </v-btn> -->
 
             <v-spacer></v-spacer>
 
@@ -74,6 +76,37 @@
             </div>
             </v-expand-transition>
         </v-card>
+        </div>
+        <div class="split">
+            <v-row v-if="sameBreedCats.length > 0">
+                <v-col
+                v-for="(cat,index) of sameBreedCats"
+                :key="index"
+                class="d-flex child-flex"
+                cols="4"
+                >
+                <v-img
+                    :src="`${cat.url}`"
+                    aspect-ratio="1"
+                    class="grey lighten-2 rounded"
+                    @click="viewFullSize(cat.url)"
+                >
+                    <template v-slot:placeholder>
+                    <v-row
+                        class="fill-height ma-0"
+                        align="center"
+                        justify="center"
+                    >
+                        <v-progress-circular
+                        indeterminate
+                        color="grey lighten-5"
+                        ></v-progress-circular>
+                    </v-row>
+                    </template>
+                </v-img>
+                </v-col>
+            </v-row>
+        </div>
     </div>
 </template>
 
@@ -81,24 +114,43 @@
     .breed-information {
         width:100%;
         display:flex;
-        flex-direction: column;
-        align-items: center;
+        flex-direction: row;
+        justify-content: center;
+        column-gap: 10px;
     }
 </style>
 
 <script>
+import axios from 'axios'
 export default {
     name:'breed-information',
     props:{
         selectedBreed:{
             type:Object,
             default:{}
-        }
+        },
     },
     data(){
         return{
             show:false,
+            sameBreedCats:[],
         }
-    }
+    },
+    methods:{
+        getCatsByBreed:async function(){
+            const apiResponse = await axios.get(`https://api.thecatapi.com/v1/images/search?breed_id=${this.selectedBreed.id}&limit=10`);
+            this.sameBreedCats = [...apiResponse.data];
+        },
+        viewFullSize(url){
+            window.open(url,'_blank');
+        }
+    },
+    watch:{
+        selectedBreed:function(newProps,oldProps){
+            if (newProps.id !== oldProps.id) {
+                this.getCatsByBreed();
+            }
+        },
+    },
 }
 </script>
